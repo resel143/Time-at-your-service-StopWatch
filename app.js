dddda1b08c98ce24ec0711129930f1573c6c2612
Reshul <53306036+resel143@users.noreply.github.com>
2021-03-26 13:10:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+let name = 'selena';
+console.log(name);
+
+let time = document.getElementById('clock').value;
+
+document.getElementById('start').addEventListener('click',function(){
+    console.log(time);
+});
